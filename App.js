f3dab1b86330ad8a3e26c7f01d8ab10147d2705a
asteriskzuo <zuoyuhsywn@hotmail.com>
2022-04-20 19:06:19 +0800

    update

--- App.js
+++ App.js
@@ -20,9 +20,7 @@ import {
   ChatClient,
   ChatOptions,
   ChatMessageChatType,
-  ChatMessageStatus,
   ChatMessage,
-  ChatMessageStatusCallback,
 } from 'react-native-chat-sdk';
 
 const App = () => {
